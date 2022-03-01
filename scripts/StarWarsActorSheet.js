e0bb08dc4d965fda3008473fa0d0abc92b192537
jessev14 <jessevo321@gmail.com>
2022-03-01 14:11:09 -0500

    complete General tab
    integrate species/careers w/ corr roll table
    
    add weakness and goal textarea

--- scripts/StarWarsActorSheet.js
+++ scripts/StarWarsActorSheet.js
@@ -16,4 +16,22 @@ export class StarWarsActorSheet extends alienrpgActorSheet {
     get template() {
         return `modules/${moduleName}/templates/starWarsActorSheetTemplate.hbs`;
     }
+
+    getData(options) {
+        const data = super.getData(options);
+
+        for (const e of ["Playable Species", "Playable Careers"]) {
+            const type = e === "Playable Species" ? "species" : "careers";
+            data[type] = {};
+            const table = game.tables.find(t => t.name === e);
+            if (!table) continue;
+            
+            table.results.contents.forEach(r => {
+                const val = r.data.text;
+                data[type][val] = val;
+            });
+        }
+
+        return data;
+    }
 }
